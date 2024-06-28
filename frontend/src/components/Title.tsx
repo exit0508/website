09a876ab0ca9810d7df33404b11155ec3443774d
exit0508 <17ad060@ms.dendai.ac.jp>
2024-06-28 09:21:29 +0900

    fix

--- frontend/src/components/Title.tsx
+++ frontend/src/components/Title.tsx
@@ -1,3 +1,33 @@
+import { Button } from "@/components/ui/button";
+import {
+  GithubIcon,
+  LucideGithub,
+  LucideLinkedin,
+  LucideMail,
+} from "lucide-react";
+
+export function ButtonGhost() {
+  return (
+    <div className="mt-5">
+      <Button variant="ghost">
+        <a href="mailto:exit.deguchi0508@gmail.com">
+          <LucideMail />
+        </a>
+      </Button>
+      <Button variant="ghost">
+        <a href="https://www.linkedin.com/in/yurina-deguchi-88854074/">
+          <LucideLinkedin />
+        </a>
+      </Button>
+      <Button variant="ghost">
+        <a href="https://github.com/exit0508">
+          <GithubIcon />
+        </a>
+      </Button>
+    </div>
+  );
+}
+
 const Title = () => {
   return (
     <>
@@ -6,6 +36,9 @@ const Title = () => {
         Aspiring designer and front-end developer passionate about creating in
         the Plureverse :)
       </h2>
+      <div>
+        <ButtonGhost />
+      </div>
     </>
   );
 };
--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -6,14 +6,47 @@ const About = () => {
       <Title />
       <div className="mt-10">
         <div>
-          <h1 className="text-2xl mb-10">Motivation</h1>
+          <h1 className="text-2xl mb-5">Motivation</h1>
+          <h2 className="text-xl mb-2">Empathy</h2>
+          <p className="text-xl mb-10">
+            Emphasis is the most essential part of my motivation. If I found
+            "ontological design" to be a first, it makes me much more certain of
+            it. There are many taken for granted for invisible existence. I
+            would like to interact with them with empathy and corperate them to
+            make a more comfortable society. I will not stick to "specific
+            means". I'll always give priority to solving problems. However,
+            because of my academic background, I have an interest in
+            human-digital interaction. I have broad interests in interface
+            design, so it would be great if I could help you with my design and
+            front-end development skills!
+          </p>
         </div>
         <div>
-          <h1 className="text-2xl mb-10">Skills</h1>
+          <h1 className="text-2xl mb-5">Skills</h1>
+          <p className="mt-3">
+            I own basic skills using below them ,and am an autonomous
+            self-learner.
+          </p>
+          <h3 className="mt-2">Tech skillset</h3>
+          <ul>
+            <li>Figma / Adobe XD</li>
+            <li>CSS / HTML</li>
+            <li>Typescript / Javascript</li>
+            <li>React</li>
+            <li>Next.js</li>
+            <li>Git</li>
+          </ul>
+          <h3 className="mt-2">Others</h3>
+          <ul>
+            <li>Japanese (NativeLevel)</li>
+            <li>English (Conversational Level)</li>
+            <li>Facilitate discussions</li>
+            <li>Qualitative analysis for user interface and experience</li>
+          </ul>
         </div>
-        <div>
+        {/* <div>
           <h1 className="text-2xl mb-10">Timeline</h1>
-        </div>
+        </div> */}
       </div>
     </>
   );
